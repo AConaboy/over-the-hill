8dff6f73e917d04c5003074cce6ba662f3401e3e
AConaboy <andrewpicotconaboy@protonmail.com>
2026-07-16 20:26:17 +0100

    Add first effort at layout

---
+++ js/navigation.js
@@ -0,0 +1,17 @@
+const menuButton = document.querySelector(".menu-button");
+const navigation = document.querySelector(".main-navigation");
+
+if (menuButton && navigation) {
+  menuButton.addEventListener("click", () => {
+    const isOpen = navigation.classList.toggle("is-open");
+
+    menuButton.setAttribute("aria-expanded", String(isOpen));
+  });
+
+  navigation.addEventListener("click", (event) => {
+    if (event.target instanceof HTMLAnchorElement) {
+      navigation.classList.remove("is-open");
+      menuButton.setAttribute("aria-expanded", "false");
+    }
+  });
+}
